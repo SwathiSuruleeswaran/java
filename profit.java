5eb3c88b553f9f19017914a5e854c3a9424d5e1c
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-07-30 21:25:02 +0530

    Create profit.java

---
+++ profit.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int x = 1000;
+        int a = 2;
+        int b = 1;
+        int c = x*a;
+        int d = x*b;
+        int e = 100;
+        int f =d-e;
+        System.out.print(f);
+    }
+}
