183353a5e5bc11b8141da24fc3c10caa4add76db
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-07-31 09:51:52 +0530

    Create reverse star patt.java

---
+++ reverse star patt.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 4;
+        for(int i=1;i<=a;i++){
+            for (int j=a;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
