0d96ec427b972b984d4e8bed50b5fc1620f0108f
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-07-31 10:32:54 +0530

    Create floyd pattern.java

---
+++ floyd pattern.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int n=1;
+        for(int i=1;i<=a;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print(n);
+                n++;
+            }
+            System.out.println();
+        }
+    }
+}
