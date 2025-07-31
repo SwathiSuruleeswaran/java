c72409154088cd50f805debdab337d69af17e98f
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-07-31 12:08:05 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int rev=0;
+        int num=12345;
+        while(num!=0){
+            int digit=num%10;
+            rev=rev*10+digit;
+            num=num/10;
+        }
+        System.out.print(rev);
+
+        }
+    }
