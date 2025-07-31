7a1528f171692bb3de9150fbdf153b959e7cc0c4
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-07-31 11:53:37 +0530

    Create add n number.java

---
+++ add n number.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int a=1;
+        int sum=0;
+        while(a<=50){
+            sum+=a;
+            
+            a++;
+        }
+            System.out.println(sum);
+        
+    }
+}
