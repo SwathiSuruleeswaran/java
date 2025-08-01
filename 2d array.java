86d8076daafdb94e8c588493a83ff86c61e84421
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-08-01 15:06:56 +0530

    Create 2d array.java

---
+++ 2d array.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+       String[] a = {"I am","a student","of PSNA"};
+       for(int i=0;i<a.length;i++){
+           System.out.print(a[i]);
+       }
+    }
+}
