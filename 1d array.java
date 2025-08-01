983e8282fbe35743b920e248ba62c468390d4560
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-08-01 15:03:33 +0530

    Create 1d array.java

---
+++ 1d array.java
@@ -0,0 +1,9 @@
+class array {
+    public static void main(String[] args) {
+        int [] myNum = {10,20,30};
+        String[] cars= {"Volvo","BMW","Ford","Mazda"};
+        System.out.println(myNum[0]);
+        System.out.println(myNum[2]);
+        System.out.println(cars[0]);
+    }
+}
