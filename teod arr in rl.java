bf049b4285d373deb5a6b3415ec69110379cc5ea
Swathi Suruleeswaran <swathis23ec@psnacet.edu.in>
2025-08-06 09:15:54 +0530

    Create teod arr in rl.java

---
+++ teod arr in rl.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int rows = sc.nextInt();
+        int columns=sc.nextInt();
+        int [][] arr= new int[rows][columns];
+        if(rows==columns){
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr.length;j++){
+                    if(i+j==columns-1){
+                        arr[i][j]=1;
+                    }else{
+                        arr[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+}
